13a409b3ade4d16d84308edea148cef13415b90d
Luan Rocha <luanrochasantiago@gmail.com>
2025-10-06 16:42:04 -0300

    Intro7

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('usuarios')->group(function() {
-     Route::get('', function() {
-        return 'usuário';
-    })->name('usuario');
-    Route::get('/{id}', function() {
-        return 'Mostra detralhes';
-    })->name('usuario.show');
-     Route::get('/{id}/tags', function() {
-        return 'Tags do usuário';
-    })->name('usuario.tags');
-       
-});
 
-Route::get('/a-empresa/{string?}', function ($string = null) {
-    return $string;
-    //return view('welcome');
-})->name('a-empresa');
+Route::get('user/{user}', [UserController::class, 'show']);
 
-Route::get('/users/{paramA}/{paramB}', function ($paramA, $paramB) {
-    return $paramA . ' - ' . $paramB;
-    //return view('welcome');
+Route::get('/', function () {
+    
+    return view('welcome');
 });
